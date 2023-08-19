03d0279befec2d914137ba1c8bdcfaea264d1c1c
BoggavarapuMahesh <136325414+BoggavarapuMahesh@users.noreply.github.com>
2023-08-19 12:06:15 +0530

    Update approvedDeposit.js

--- scripts/approvedDeposit.js
+++ scripts/approvedDeposit.js
@@ -1,58 +1,69 @@
+//import c from "contracts/examples/erc721-transfer/FxERC721RootTunnel.sol"// Import necessary packages and contracts
 const { ethers } = require("hardhat");
 const { FXRootContractAbi } = require("../artifacts/FXRootContractAbi.js");
-const ABI = require("../artifacts/contracts/mahesh.sol/mahesh.json"); // Import ABI for the "mahesh" contract
+const ABI = require("../artifacts/contracts/mahesh.sol/mahesh.json");
 require("dotenv").config();
 
+//Transfer ERC721A tokens to the Ethereum FxChain network
 async function main() {
-  try {
-    // Set up the Ethereum network provider and private key
-    const networkAddress = "https://eth-goerli.g.alchemy.com/v2/h-asDNMJ21mVniDAY3XE1VQ9F7PW7A0x";
-    const privateKey = process.env.PRIVATE_KEY;
-    const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+  // Set up connections to network and wallet
+  const networkAddress =
+    "https://eth-goerli.g.alchemy.com/v2/h-asDNMJ21mVniDAY3XE1VQ9F7PW7A0x";
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-    // Create a wallet instance and retrieve the signer
-    const wallet = new ethers.Wallet(privateKey, provider);
-    const [signer] = await ethers.getSigners();
+  // Create a wallet instance
+  const wallet = new ethers.Wallet(privateKey, provider);
 
-    // Get the contract factory for the "mahesh" NFT contract
-    const NFT = await ethers.getContractFactory("mahesh");
-    const nft = await NFT.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3");
+  // Get the signer instance
+  const [signer] = await ethers.getSigners();
 
-    // Configure the FXRoot contract
-    const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
-    const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
+  // Get ERC721A contract instance
+  const NFT = await ethers.getContractFactory("mahesh");
+  const nft = await NFT.attach("0x7E24D2D6738032355Fc89E7BE2Dcd21dd36d69D0");
 
-    // Token IDs to be transferred
-    const tokenIds = [0, 1, 2, 3, 4];
-    const gasLimit = 2000000; // Set an appropriate gas limit for the transaction
+  // Get FXRoot contract instance
+  const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
+  const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
 
-    // Approve the "mahesh" NFT contract for transfer
-    const approveTx = await nft.connect(signer).setApprovalForAll(fxRootAddress, true);
-    await approveTx.wait();
-    console.log("Approval confirmed");
+  // TokenIds to transfer
+  const tokenIds = [0, 1, 2, 3, 4];
 
-    // Iterate through token IDs and deposit to FXRoot contract
-    for (let i = 0; i < tokenIds.length; i++) {
-      const depositTx = await fxRoot
-        .connect(signer)
-        .deposit(nft.address, wallet.address, tokenIds[i], "0x6566", { gasLimit });
-      await depositTx.wait();
-    }
+  // Approve the nfts for transfer
+  const approveTx = await nft
+    .connect(signer)
+    .setApprovalForAll(fxRootAddress, true);
+  await approveTx.wait();
+  console.log("Approval confirmed");
 
-    console.log("Approved and deposited");
+  // Deposit the nfts to the FXRoot contracts
+  for (let i = 0; i < tokenIds; i++) {
+    const depositTx = await fxRoot
+      .connect(signer)
+      .deposit(nft.address, wallet.address, tokenIds[i], "0x6566");
 
-    // Check the updated balance of "mahesh" NFTs in the wallet
-    const balance = await nft.balanceOf(wallet.address);
-    console.log(
-      "NFT wallet balance",
-      wallet.address,
-      "is:",
-      balance.toString()
-    );
-  } catch (error) {
-    console.error("Error:", error);
-    process.exit(1);
+    // Wait for the deposit to be confirmed
+    await depositTx.wait();
   }
+
+  console.log("Approved and deposited");
+
+  // Test balanceOf
+  const balance = await nft.balanceOf(wallet.address);
+
+  // Print the balance of the wallet
+  console.log(
+    "wallet balance",
+    wallet.address,
+    "is: ",
+    balance.toString()
+  );
 }
 
-main();
+// Call the main function and handle any errors
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
